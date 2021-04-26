44aa9edc3f868f7320630033829d6792b61f945b
mimamulhassankhan <pappuhassan440@gmail.com>
2021-04-27 01:14:38 +0600

    [Red Onion Ltd]: Stripe.js added
    Signed-off-by: mimamulhassankhan <pappuhassan440@gmail.com>

--- hot-onion-restaurant-client/src/Components/Cart/Cart.js
+++ hot-onion-restaurant-client/src/Components/Cart/Cart.js
@@ -1,30 +1,101 @@
 import { Text } from '@fluentui/react';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router';
 import { addToCart, setAllOrders } from '../../Redux/Actions/RestaurantActions';
 import MiniProductForCart from '../MiniProductForCart/MiniProductForCart';
 import emailjs from 'emailjs-com';
 import './Cart.css';
-require('dotenv').config()
+import { CardCvcElement, CardExpiryElement, CardNumberElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import useResponsiveFontSize from './useResponsiveFontSize';
+require('dotenv').config();
+
+const useOptions = () => {
+    const fontSize = useResponsiveFontSize();
+    const options = useMemo(
+      () => ({
+        style: {
+          base: {
+            fontSize,
+            color: "#424770",
+            letterSpacing: "0.025em",
+            fontFamily: "Source Code Pro, monospace",
+            "::placeholder": {
+              color: "#aab7c4"
+            }
+          },
+          invalid: {
+            color: "#9e2146"
+          }
+        }
+      }),
+      [fontSize]
+    );
+  
+    return options;
+};
 
 const Cart = ({cart, user, addToCart, allOrders, setAllOrders, buttonDisabledState, allSuppliers}) => {
     const history = useHistory();
+    const stripe = useStripe();
+    const elements = useElements();
+    const options = useOptions();
+
+    const [paymentMethodology, setPaymentMethodology] = useState('cashOnDelivery');
+    const [cardError, setCardError] = useState(paymentMethodology === 'cashOnDelivery' ? false : true);
+
     const subTotal = cart.reduce((st, pd) => st += (pd.foodPrice * pd.quantity), 0);
     const tax = subTotal * .015;
     const delivary = subTotal * .06;
     const total = subTotal + tax + delivary;
 
+    const processPaymentInformation = async () => {
+        if(paymentMethodology === 'cashOnDelivery'){
+            const paymentDetails = {
+                paymentStatus: 'pending',
+                paymentType: 'Cash on delivery',
+                paymentId: Math.random().toString()
+            }
 
-    const handlePlaceOrder = () =>{
+            return paymentDetails;
+        }
+        else if(paymentMethodology === 'cardPayment'){
+            const {error, paymentMethod} = await stripe.createPaymentMethod({
+                type: 'card',
+                card: elements.getElement(CardNumberElement),
+            });
+        
+            if (error) {
+                console.log('[error]', error);
+            } else {
+                console.log('[PaymentMethod]', paymentMethod);
+                const paymentDetails = {
+                    paymentStatus: 'paid',
+                    paymentType: 'Card Payment',
+                    paymentId: paymentMethod.id
+                }
+                return paymentDetails;
+            }
+        }
+    }
+
+
+    const handlePlaceOrder = async () =>{
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const payment = await processPaymentInformation();
         const [selectSupplier] = allSuppliers.filter(supplier => supplier.deliveryPersonActiveStatus === 'active');
+
         const orderDetails = {
             userId: user._id,
             userMail: user.email,
             cartDetails: cart,
             supplierDetails: selectSupplier,
-            paymentStatus: 'pending',
-            paymentType: 'Cash on delivery',
+            paymentStatus: payment.paymentStatus,
+            paymentType: payment.paymentType,
+            paymentId: payment.paymentId,
             orderStatus: 'pending'
         }
 
@@ -52,9 +123,52 @@ const Cart = ({cart, user, addToCart, allOrders, setAllOrders, buttonDisabledSta
         <>
             <h6>Payment Method</h6>
             <label className="container_div">Cash On Delivary
-                <input type="radio" checked="checked" name="radio" />
+                <input type="radio" checked={paymentMethodology === 'cashOnDelivery'} onChange={() => setPaymentMethodology('cashOnDelivery')} name="radio" />
                 <span className="checkmark_div"></span>
             </label>
+            <label className="container_div">Card Payment
+                <input type="radio" onChange={() => setPaymentMethodology('cardPayment')} name="radio" />
+                <span className="checkmark_div"></span>
+            </label>
+
+            {
+                paymentMethodology === 'cardPayment' && 
+                <>
+                    <label>
+                        Card number
+                        <CardNumberElement
+                            options={options}
+                            onChange={({error}) => {
+                                error ? setCardError(true) : setCardError(false);
+                            }}
+                        />
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        Expiration date
+                        <CardExpiryElement
+                            options={options}
+                            onChange={({error}) => {
+                                error ? setCardError(true) : setCardError(false);
+                            }}
+                        />
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        CVC
+                        <CardCvcElement
+                            options={options}
+                            onChange={({error}) => {
+                                error ? setCardError(true) : setCardError(false);
+                            }}
+                        />
+                    </label>
+                    <br />
+                    <br />
+                </>
+            }
             {
                 cart.map(pd => <MiniProductForCart toCart={addToCart} data={pd} key={pd._id}></MiniProductForCart>)
             }
@@ -73,7 +187,7 @@ const Cart = ({cart, user, addToCart, allOrders, setAllOrders, buttonDisabledSta
                 </div>
             </div>
             <br />
-            <input className="btn btn-danger rounded-pill btn-block" onClick={() => handlePlaceOrder()} type="button" value="Place Order" disabled={buttonDisabledState || cart.length <=0}/>
+            <input className="btn btn-danger rounded-pill btn-block" onClick={() => handlePlaceOrder()} type="button" value="Place Order" disabled={buttonDisabledState || cart.length <=0 || cardError }/>
         </>
     );
 };
--- hot-onion-restaurant-client/src/index.js
+++ hot-onion-restaurant-client/src/index.js
@@ -6,15 +6,21 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { restaurantStore } from './Redux/Stores/RestaurantStore';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51HadI3J6JIJmmHgl61TslmWB2Tq2VILwkCObBIaO3Y4mvIHWkDa4SvBSEgn9K3FbZsQfms1U89VHmJsnYKvcfQyF00UkikFDVR');
 
 ReactDOM.render(
-  <Provider store={restaurantStore}>
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
-  </Provider>,
+  <Elements stripe={stripePromise}>
+    <Provider store={restaurantStore}>
+      <React.StrictMode>
+        <Router>
+          <App />
+        </Router>
+      </React.StrictMode>
+    </Provider>
+  </Elements>,
   document.getElementById('root')
 );
 
